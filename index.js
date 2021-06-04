cb790336efac5197b17bf9657574ce2d4891269e
Wilson Qiu <Wilsonqiu61@yahoo.com>
2021-06-03 19:45:20 -0700

    removed token

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL","REACTION"]});
 const token = process.env.TOKEN
-// const token = 'ODQ0NzEyOTY0OTI1MTYxNDcz.YKWaQA.ps0t5qmYx3z_sGmQxc3iigNgmsc' 
 
 const prefix = '!';
 
